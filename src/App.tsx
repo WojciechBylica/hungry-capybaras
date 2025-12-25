cd1955527ea8e8f8408833d729ed216898d95781
Wojciech Bylica <wojtekbyl@gmail.com>
2025-12-25 15:25:05 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Box, Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <Box>
+      <Typography variant='h1'>Kapibry - głodomory</Typography>
+      <Typography>Rozwiąż zadania i nakarm głodną kapibarę</Typography>
+    </Box>
+  )
+}
+
+export default App
